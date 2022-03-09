cc36ce01fce69430fb1dc6091f8867eff24335c4
Dmitrii Sirota <65736946+Eeveeee@users.noreply.github.com>
2022-03-10 01:04:39 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Конфиг
+const serverTimeout = 600;
+const closeBtnClassname = '.closeButton-mupH76'; //Класс кнопки закрытия ЛС
+const leaveBtnClassnames =
+  'button-f2h6uQ lookFilled-yCfaCM colorRed-rQXKgM sizeMedium-2bFIHr grow-2sR_-F'; //Класс кнопки покидания в модальном окне
+
+function leaveGroup() {
+  const closeBtn = document.querySelector(closeBtnClassname);
+  if (closeBtn) {
+    closeBtn.click();
+    const leaveBtn = Array.from(
+      document.getElementsByClassName(leaveBtnClassnames)
+    )[0];
+    if (leaveBtn) {
+      leaveBtn.click();
+    }
+    setTimeout(() => {
+      // Проверка на наличие кнопок
+      const emptyCheck = !!document.querySelector('.closeButton-mupH76');
+      if (emptyCheck) {
+        leaveGroup();
+      }
+    }, serverTimeout);
+  }
+}
+leaveGroup();
